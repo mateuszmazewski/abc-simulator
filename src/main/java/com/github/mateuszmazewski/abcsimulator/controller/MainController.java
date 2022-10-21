71eeb74ad824ba04ae1ef2e6358c029ccaeda7a6
Mateusz Mazewski <mateusz.mazewski.stud@pw.edu.pl>
2022-10-21 19:13:27 +0200

    Add top menu bar functionality -- close, about

--- src/main/java/com/github/mateuszmazewski/abcsimulator/controller/MainController.java
+++ src/main/java/com/github/mateuszmazewski/abcsimulator/controller/MainController.java
@@ -1,8 +1,13 @@
 package com.github.mateuszmazewski.abcsimulator.controller;
 
+import com.github.mateuszmazewski.abcsimulator.utils.DialogUtils;
 import com.github.mateuszmazewski.abcsimulator.utils.FxmlUtils;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.BorderPane;
+
+import java.util.Optional;
 
 public class MainController {
 
@@ -11,5 +16,22 @@ public class MainController {
 
     public void setCenter(String fxmlPath) {
         borderPane.setCenter(FxmlUtils.loadFxml(fxmlPath));
+    }
+
+    @FXML
+    private void closeApp() {
+        Optional<ButtonType> buttonType = DialogUtils.showConfirmDialog(
+                "exitDialog.title",
+                "exitDialog.header",
+                null);
+        if (buttonType.isPresent() && buttonType.get() == ButtonType.OK) {
+            Platform.exit();
+            System.exit(0);
+        }
+    }
+
+    @FXML
+    private void showAboutDialog() {
+        DialogUtils.showAboutDialog();
     }
 }
--- src/main/java/com/github/mateuszmazewski/abcsimulator/utils/DialogUtils.java
+++ src/main/java/com/github/mateuszmazewski/abcsimulator/utils/DialogUtils.java
@@ -1,20 +1,50 @@
 package com.github.mateuszmazewski.abcsimulator.utils;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class DialogUtils {
 
     private static final ResourceBundle messagesBundle = FxmlUtils.getResourceBundle();
+
     public static void errorDialog(String msg) {
         Alert error = new Alert(Alert.AlertType.ERROR);
+
         error.setTitle(messagesBundle.getString("errorDialog.title"));
         error.setHeaderText(messagesBundle.getString("errorDialog.header"));
         TextArea textArea = new TextArea(msg);
         textArea.setEditable(false);
         error.getDialogPane().setContent(textArea);
         error.showAndWait();
+    }
+
+    public static Optional<ButtonType> showConfirmDialog(String titleKey, String headerTextKey, String contextTextKey) {
+        Alert confirm = new Alert(Alert.AlertType.CONFIRMATION);
+
+        try {
+            confirm.setTitle(messagesBundle.getString(titleKey));
+            confirm.setHeaderText(messagesBundle.getString(headerTextKey));
+            if (contextTextKey != null && !contextTextKey.isEmpty()) {
+                confirm.setContentText(messagesBundle.getString(contextTextKey));
+            }
+        } catch (Exception e) {
+            errorDialog(e.getMessage());
+            return Optional.empty();
+        }
+
+        return confirm.showAndWait();
+    }
+
+    public static void showAboutDialog() {
+        Alert about = new Alert(Alert.AlertType.INFORMATION);
+
+        about.setTitle(messagesBundle.getString("aboutDialog.title"));
+        about.setHeaderText(messagesBundle.getString("aboutDialog.header"));
+        about.setContentText(messagesBundle.getString("aboutDialog.content"));
+        about.showAndWait();
     }
 }
