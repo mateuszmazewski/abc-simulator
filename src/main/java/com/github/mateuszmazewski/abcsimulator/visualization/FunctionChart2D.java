9a3da659cbc59e66590bd7890e9caad6c894fb14
Mateusz Mazewski <mateusz.mazewski.stud@pw.edu.pl>
2022-11-07 20:39:04 +0100

    Remove unnecessary "this" keywords

--- src/main/java/com/github/mateuszmazewski/abcsimulator/visualization/FunctionChart2D.java
+++ src/main/java/com/github/mateuszmazewski/abcsimulator/visualization/FunctionChart2D.java
@@ -38,79 +38,79 @@ public class FunctionChart2D extends GridPane {
             throw new IllegalArgumentException("Cannot visualize function with dimension other than 2");
         }
 
-        this.x1 = testFunction.getLowerBoundaries()[0];
-        this.x2 = testFunction.getUpperBoundaries()[0];
-        this.y1 = testFunction.getLowerBoundaries()[1];
-        this.y2 = testFunction.getUpperBoundaries()[1];
+        x1 = testFunction.getLowerBoundaries()[0];
+        x2 = testFunction.getUpperBoundaries()[0];
+        y1 = testFunction.getLowerBoundaries()[1];
+        y2 = testFunction.getUpperBoundaries()[1];
 
-        this.chartCanvas = new Canvas();
-        this.xAxisCanvas = new Canvas();
-        this.yAxisCanvas = new Canvas();
-        this.pixelWriter = this.chartCanvas.getGraphicsContext2D().getPixelWriter();
-        this.chartCanvas.widthProperty().bind(this.chartCanvas.heightProperty()); // chartCanvas is square
-        this.chartCanvas.heightProperty().bind(this.heightProperty().multiply(0.9));
-        this.chartCanvasWidth = (int) this.chartCanvas.getWidth();
-        this.chartCanvasHeight = (int) this.chartCanvas.getHeight();
+        chartCanvas = new Canvas();
+        xAxisCanvas = new Canvas();
+        yAxisCanvas = new Canvas();
+        pixelWriter = chartCanvas.getGraphicsContext2D().getPixelWriter();
+        chartCanvas.widthProperty().bind(chartCanvas.heightProperty()); // chartCanvas is square
+        chartCanvas.heightProperty().bind(heightProperty().multiply(0.9));
+        chartCanvasWidth = (int) chartCanvas.getWidth();
+        chartCanvasHeight = (int) chartCanvas.getHeight();
 
-        this.xAxisCanvas.widthProperty().bind(this.chartCanvas.widthProperty());
-        this.yAxisCanvas.heightProperty().bind(this.chartCanvas.heightProperty());
-        this.xAxisCanvas.setHeight(this.axesMarkerLength + this.axesFontSize + this.axesGapBetweenMarkerAndText);
-        this.yAxisCanvas.setWidth(this.axesMarkerLength + this.axesGapBetweenMarkerAndText + this.getLongestYTextWidth());
+        xAxisCanvas.widthProperty().bind(chartCanvas.widthProperty());
+        yAxisCanvas.heightProperty().bind(chartCanvas.heightProperty());
+        xAxisCanvas.setHeight(axesMarkerLength + axesFontSize + axesGapBetweenMarkerAndText);
+        yAxisCanvas.setWidth(axesMarkerLength + axesGapBetweenMarkerAndText + getLongestYTextWidth());
 
-        this.getChildren().addAll(this.chartCanvas, this.xAxisCanvas, this.yAxisCanvas);
-        GridPane.setHalignment(this.xAxisCanvas, HPos.RIGHT);
-        GridPane.setValignment(this.yAxisCanvas, VPos.TOP);
-        GridPane.setConstraints(this.chartCanvas, 1, 0, 1, 1);
-        GridPane.setConstraints(this.xAxisCanvas, 0, 1, 2, 1);
-        GridPane.setConstraints(this.yAxisCanvas, 0, 0, 1, 2);
+        getChildren().addAll(chartCanvas, xAxisCanvas, yAxisCanvas);
+        GridPane.setHalignment(xAxisCanvas, HPos.RIGHT);
+        GridPane.setValignment(yAxisCanvas, VPos.TOP);
+        GridPane.setConstraints(chartCanvas, 1, 0, 1, 1);
+        GridPane.setConstraints(xAxisCanvas, 0, 1, 2, 1);
+        GridPane.setConstraints(yAxisCanvas, 0, 0, 1, 2);
 
         updateFuncValuesRange();
         visualizeFunc();
         drawAxes();
 
         ChangeListener<Number> paneSizeListener = (observable, oldValue, newValue) -> {
-            this.chartCanvasWidth = (int) this.chartCanvas.getWidth();
-            this.chartCanvasHeight = (int) this.chartCanvas.getHeight();
+            chartCanvasWidth = (int) chartCanvas.getWidth();
+            chartCanvasHeight = (int) chartCanvas.getHeight();
             updateFuncValuesRange();
             visualizeFunc();
             drawAxes();
         };
 
-        this.widthProperty().addListener(paneSizeListener);
-        this.heightProperty().addListener(paneSizeListener);
+        widthProperty().addListener(paneSizeListener);
+        heightProperty().addListener(paneSizeListener);
     }
 
     private double getLongestYTextWidth() {
         // Longest number on y-axis is a negative number with two digits after decimal point
-        double longestNumberOnYAxis = -((int) Math.max(Math.abs(this.y1), Math.abs(this.y2)) + 0.77);
+        double longestNumberOnYAxis = -((int) Math.max(Math.abs(y1), Math.abs(y2)) + 0.77);
         Text longestYText = new Text(String.valueOf(longestNumberOnYAxis));
-        longestYText.setFont(this.axesFont);
+        longestYText.setFont(axesFont);
         return longestYText.getLayoutBounds().getWidth();
     }
 
     private void drawAxes() {
         int stepsCount = 10;
-        double xAxisStep = this.xAxisCanvas.getWidth() / stepsCount;
-        double yAxisStep = this.yAxisCanvas.getHeight() / stepsCount;
-        double xFuncStep = (this.x2 - this.x1) / stepsCount;
-        double yFuncStep = (this.y2 - this.y1) / stepsCount;
+        double xAxisStep = xAxisCanvas.getWidth() / stepsCount;
+        double yAxisStep = yAxisCanvas.getHeight() / stepsCount;
+        double xFuncStep = (x2 - x1) / stepsCount;
+        double yFuncStep = (y2 - y1) / stepsCount;
 
-        GraphicsContext xAxisGraphics = this.xAxisCanvas.getGraphicsContext2D();
-        GraphicsContext yAxisGraphics = this.yAxisCanvas.getGraphicsContext2D();
+        GraphicsContext xAxisGraphics = xAxisCanvas.getGraphicsContext2D();
+        GraphicsContext yAxisGraphics = yAxisCanvas.getGraphicsContext2D();
         xAxisGraphics.setStroke(Color.BLACK);
         yAxisGraphics.setStroke(Color.BLACK);
 
         double x, y;
         String xText, yText;
-        xAxisGraphics.setFont(this.axesFont);
-        yAxisGraphics.setFont(this.axesFont);
+        xAxisGraphics.setFont(axesFont);
+        yAxisGraphics.setFont(axesFont);
 
-        xAxisGraphics.clearRect(0, 0, this.xAxisCanvas.getWidth(), this.xAxisCanvas.getHeight());
-        yAxisGraphics.clearRect(0, 0, this.yAxisCanvas.getWidth(), this.yAxisCanvas.getHeight());
+        xAxisGraphics.clearRect(0, 0, xAxisCanvas.getWidth(), xAxisCanvas.getHeight());
+        yAxisGraphics.clearRect(0, 0, yAxisCanvas.getWidth(), yAxisCanvas.getHeight());
 
         for (int i = 0; i <= stepsCount; i++) {
-            xText = String.valueOf(roundToTwoDecimalPlaces(this.x1 + i * xFuncStep));
-            yText = String.valueOf(roundToTwoDecimalPlaces(this.y1 + (stepsCount - i) * yFuncStep));
+            xText = String.valueOf(roundToTwoDecimalPlaces(x1 + i * xFuncStep));
+            yText = String.valueOf(roundToTwoDecimalPlaces(y1 + (stepsCount - i) * yFuncStep));
 
             xAxisGraphics.setTextBaseline(VPos.BOTTOM);
             yAxisGraphics.setTextAlign(TextAlignment.RIGHT);
@@ -139,42 +139,42 @@ public class FunctionChart2D extends GridPane {
                 yAxisGraphics.setTextBaseline(VPos.CENTER);
             }
 
-            xAxisGraphics.strokeLine(x, 0, x, this.axesMarkerLength);
-            xAxisGraphics.fillText(xText, x, this.axesMarkerLength + this.axesFontSize + this.axesGapBetweenMarkerAndText);
+            xAxisGraphics.strokeLine(x, 0, x, axesMarkerLength);
+            xAxisGraphics.fillText(xText, x, axesMarkerLength + axesFontSize + axesGapBetweenMarkerAndText);
 
-            yAxisGraphics.strokeLine(this.yAxisCanvas.getWidth(), y, this.yAxisCanvas.getWidth() - this.axesMarkerLength, y);
-            yAxisGraphics.fillText(yText, this.yAxisCanvas.getWidth() - (this.axesMarkerLength + this.axesGapBetweenMarkerAndText), y);
+            yAxisGraphics.strokeLine(yAxisCanvas.getWidth(), y, yAxisCanvas.getWidth() - axesMarkerLength, y);
+            yAxisGraphics.fillText(yText, yAxisCanvas.getWidth() - (axesMarkerLength + axesGapBetweenMarkerAndText), y);
         }
     }
 
     private void visualizeFunc() {
-        for (int xCanvas = 0; xCanvas < this.chartCanvasWidth; xCanvas++) {
-            for (int yCanvas = 0; yCanvas < this.chartCanvasHeight; yCanvas++) {
+        for (int xCanvas = 0; xCanvas < chartCanvasWidth; xCanvas++) {
+            for (int yCanvas = 0; yCanvas < chartCanvasHeight; yCanvas++) {
                 double funcVal = getFuncVal(xCanvas, yCanvas);
                 // Scale func. values to [0, 240] ->  HSV color space
                 // 0 deg. = red, 120 deg. = green, 240 deg. = blue
-                double funcValScaled = ((funcVal - this.funcMinValue) / (this.funcMaxValue - this.funcMinValue) * 240);
+                double funcValScaled = ((funcVal - funcMinValue) / (funcMaxValue - funcMinValue) * 240);
                 Color color = Color.hsb(240 - funcValScaled, 1.0, 1.0);
-                this.pixelWriter.setColor(xCanvas, yCanvas, color);
+                pixelWriter.setColor(xCanvas, yCanvas, color);
             }
         }
     }
 
     private void updateFuncValuesRange() {
-        for (int xCanvas = 0; xCanvas < this.chartCanvasWidth; xCanvas++) {
-            for (int yCanvas = this.chartCanvasHeight - 1; yCanvas >= 0; yCanvas--) {
+        for (int xCanvas = 0; xCanvas < chartCanvasWidth; xCanvas++) {
+            for (int yCanvas = chartCanvasHeight - 1; yCanvas >= 0; yCanvas--) {
                 double funcVal = getFuncVal(xCanvas, yCanvas);
-                this.funcMinValue = Math.min(this.funcMinValue, funcVal);
-                this.funcMaxValue = Math.max(this.funcMaxValue, funcVal);
+                funcMinValue = Math.min(funcMinValue, funcVal);
+                funcMaxValue = Math.max(funcMaxValue, funcVal);
             }
         }
     }
 
     private double getFuncVal(int xCanvas, int yCanvas) {
         // Scale canvas' coords to function's coords
-        double funcX = (double) xCanvas / this.chartCanvasWidth * (this.x2 - this.x1) + this.x1;
-        double funcY = (double) yCanvas / this.chartCanvasHeight * (this.y2 - this.y1) + this.y1;
-        return this.testFunction.getValue(new double[]{funcX, funcY});
+        double funcX = (double) xCanvas / chartCanvasWidth * (x2 - x1) + x1;
+        double funcY = (double) yCanvas / chartCanvasHeight * (y2 - y1) + y1;
+        return testFunction.getValue(new double[]{funcX, funcY});
     }
 
 }
