fb84d718ae5e93ba9b519e06551dc941957b2c36
Mateusz Mazewski <mateusz.mazewski.stud@pw.edu.pl>
2023-01-04 14:34:48 +0100

    Fix: call initOwner() for each dialog

--- src/main/java/com/github/mateuszmazewski/abcsimulator/utils/DialogUtils.java
+++ src/main/java/com/github/mateuszmazewski/abcsimulator/utils/DialogUtils.java
@@ -1,5 +1,6 @@
 package com.github.mateuszmazewski.abcsimulator.utils;
 
+import com.github.mateuszmazewski.abcsimulator.controller.ControllerMediator;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
@@ -12,6 +13,7 @@ public class DialogUtils {
 
     public static void errorDialog(String msg) {
         Alert error = new Alert(Alert.AlertType.ERROR);
+        error.initOwner(ControllerMediator.getInstance().mainControllerGetStage());
 
         error.titleProperty().bind(messagesFactory.getStringBinding("errorDialog.title"));
         error.headerTextProperty().bind(messagesFactory.getStringBinding("errorDialog.header"));
@@ -23,6 +25,7 @@ public class DialogUtils {
 
     public static Optional<ButtonType> showConfirmDialog(String titleKey, String headerTextKey, String contextTextKey) {
         Alert confirm = new Alert(Alert.AlertType.CONFIRMATION);
+        confirm.initOwner(ControllerMediator.getInstance().mainControllerGetStage());
 
         try {
             confirm.titleProperty().bind(messagesFactory.getStringBinding(titleKey));
@@ -40,6 +43,7 @@ public class DialogUtils {
 
     public static void showAboutDialog() {
         Alert about = new Alert(Alert.AlertType.INFORMATION);
+        about.initOwner(ControllerMediator.getInstance().mainControllerGetStage());
 
         about.titleProperty().bind(messagesFactory.getStringBinding("aboutDialog.title"));
         about.headerTextProperty().bind(messagesFactory.getStringBinding("aboutDialog.header"));
