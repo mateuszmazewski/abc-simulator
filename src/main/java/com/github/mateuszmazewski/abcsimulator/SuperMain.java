6529a16725be2aac54c53a8d0709d86055f63bfb
Mateusz Mazewski <mateusz.mazewski.stud@pw.edu.pl>
2023-01-04 16:28:45 +0100

    Add JavaFx dependencies and SuperMain

---
+++ src/main/java/com/github/mateuszmazewski/abcsimulator/SuperMain.java
@@ -0,0 +1,9 @@
+package com.github.mateuszmazewski.abcsimulator;
+
+// This class exists in order to be able to run the app from the *.jar file on java11+
+public class SuperMain {
+
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
